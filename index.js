56146ae6648c53e1e16f79c7126ed4940ce124f8
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-07-22 14:03:13 -0300

    ajustes finais do projeto

--- index.js
+++ index.js
@@ -19,12 +19,23 @@ form.addEventListener('submit', function (e) {
     }
 
     const imc = getImc(peso, altura);
-    console.log(imc)
+    const indiceImc = getIndiceImc(imc);
+
+    const msg = `Seu IMC é ${imc} (${indiceImc}).`;
+
+    setResultado(msg, true);
 });
 
 function getIndiceImc(imc) {
     const indice = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
         'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+        if(imc >= 39.9) return indice[5];
+        if(imc >= 34.9) return indice[4];
+        if(imc >= 29.9) return indice[3];
+        if(imc >= 24.9) return indice[2];
+        if(imc >= 18.5) return indice[1];        
+        if(imc < 18.5)  return indice[0];
 };
 
 function getImc(peso, altura) {
@@ -41,7 +52,15 @@ function createP() {
 function setResultado(mensagem, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
+
     const p = createP();
+
+    if(isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+
     p.innerHTML = mensagem;
     resultado.appendChild(p);
 }
