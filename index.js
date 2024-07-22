4233030a65eec4f30701d1dc53a02f68197149df
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-07-22 13:22:59 -0300

    continuação de validações e funções

--- index.js
+++ index.js
@@ -2,15 +2,46 @@ const form = document.querySelector('#form');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('ola');
-    setResultado('olaaa');
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if(!peso) {
+        setResultado('peso inválido', false);
+        return;
+    }
+
+    if(!altura) {
+        setResultado('altura inválida', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    console.log(imc)
 });
 
-function setResultado(mensagem) {
+function getIndiceImc(imc) {
+    const indice = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
+        'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+};
+
+function getImc(peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+
+};
+
+function createP() {
+    const p = document.createElement('p');
+    return p;
+};
+
+function setResultado(mensagem, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
-    const p = document.createElement('p');
-    p.classList.add('paragrafo-resultado');
-    p.innerHTML = 'qualquer coisa';
+    const p = createP();
+    p.innerHTML = mensagem;
     resultado.appendChild(p);
 }
